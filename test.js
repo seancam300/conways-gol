d3b781610f79f5f16d6d0b1524fb11be376a3a45
sean smith <seancam300@gmail.com>
2013-04-15 17:04:50 -0700

    More testing

--- test.js
+++ test.js
@@ -1,5 +1,23 @@
-console.log("names: ");
+// creation
 var names = new Array();
+
+// populate array
+names["hat"] = "Kalima";
+names["dog"] = "banana";
 names[0] = "Sean Smith";
-names[1] = "Colton Philips";
-console.log(names);
+names[1] = "Colton Phillips";
+names[2] = "Danny B"
+
+// alter the array
+names.push(7);
+
+console.log("names: ", names);
+//names.remove("Danny B"); What! I don't know how!""
+// "How about we do it like this!""
+var index = names.indexOf("Danny B");
+names.splice(index, 1)
+console.log("Danny B stinks", names);
+
+// output
+console.log("length", names.length);
+console.log("index of Colton", names.indexOf("Colton Phillips"));
